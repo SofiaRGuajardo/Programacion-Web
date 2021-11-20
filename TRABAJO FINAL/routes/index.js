29b4e3d7c8a2668b61f537bfd4e4fffbd1f6e785
Sofia Guajardo <sofiarg94@gmail.com>
2021-11-20 07:57:06 -0300

    tp final

---
+++ TRABAJO FINAL/routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var consulta = req.body.consulta;
+
+  var obj ={
+    to: 'sofiarg94@gmail.com',
+    subject: 'Consulta desde la ViajArg',
+    html: nombre + " se contactó a través de ViajArg y realizó la siguiente consulta: <br>" +  consulta +"<br>Su email de contacto es: " + email
+  }
+
+var transport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+})
+
+var info = await transport.sendMail(obj);
+
+res.render('index',{
+  message: 'Consulta enviada',
+});
+
+});
+
+module.exports = router;
