1fafc40d58e072c47fa82a150e9019851fcc0aaf
Sofia Guajardo <sofiarg94@gmail.com>
2021-11-21 13:26:17 -0300

    agregar y modificar

--- TRABAJO FINAL/app.js
+++ TRABAJO FINAL/app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var session = require('express-session');
 require('dotenv').config();
+var session = require('express-session');
 
 var pool = require('./models/bd');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var adminRouter = require('./routes/admin/novedades');
 
@@ -26,16 +26,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(session({
-//   secret: 'contraseñaviajarg',
-//   resave: false,
-//   saveUninitialized: true
-// }));
+app.use(session({
+  secret: 'contraseñaviajarg',
+  resave: false,
+  saveUninitialized: true
+}));
+
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else{
+      res.redirect('/admin/login')
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
-app.use('/admin/novedades', adminRouter);
+app.use('/admin/novedades', secured, adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
